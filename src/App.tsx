aabbf50dc1e6c2fd6d00e0e073303f8725839573
web3-dar <onlineagentalmashell@gmail.com>
2024-11-28 02:41:03 +0100

    lol

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home/home';
 import Header from './Pages/Home/header';
 import Listing from './Pages/Listing/Listing';
